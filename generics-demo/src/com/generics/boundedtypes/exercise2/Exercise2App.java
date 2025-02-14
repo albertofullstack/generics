cd20b56ca217f18c235d5a812984703a15a0e91a
Alberto Morante <amoranteb@minsait.com>
2025-02-14 12:30:45 +0100

    Improves exercise 2.

--- generics-demo/src/com/generics/boundedtypes/exercise2/Exercise2App.java
+++ generics-demo/src/com/generics/boundedtypes/exercise2/Exercise2App.java
@@ -2,24 +2,15 @@ package com.generics.boundedtypes.exercise2;
 
 public class Exercise2App {
 
-  public static <T extends Number, V extends Number> T add(T num1, V num2) {
-    if (num1 == null || num2 == null) {
-      return null;
-    }
-
-    if (num1 instanceof Double) {
-      return (T) Double.valueOf(Double.sum(num1.doubleValue(), num2.doubleValue()));
-    } else if (num1 instanceof Integer) {
-      return (T) Integer.valueOf(num1.intValue() + num2.intValue());
-    } else if (num1 instanceof Float) {
-      return (T) Float.valueOf(num1.floatValue() + num2.floatValue());
-    } else {
-      throw new IllegalArgumentException("Type " + num1.getClass() + " is not supported by this method");
-    }
+  public static <T extends Number, V extends Number> double add(T num1, V num2) {
+    return num1.doubleValue() + num2.doubleValue();
   }
 
   public static void main(String[] args) {
 
     System.out.println(Exercise2App.add(2.4d, 5d));
+    System.out.println(Exercise2App.add(4, 5f));
+    System.out.println(Exercise2App.add(3, 5));
+    System.out.println(Exercise2App.add(2L, 3.33434));
   }
 }
