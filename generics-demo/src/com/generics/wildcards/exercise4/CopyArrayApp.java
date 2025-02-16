1837f651dcaac1ec8544a0c2968048cf2cc573c0
Alberto Morante <amoranteb@minsait.com>
2025-02-16 14:40:47 +0100

    Solves exercise 4.

---
+++ generics-demo/src/com/generics/wildcards/exercise4/CopyArrayApp.java
@@ -0,0 +1,26 @@
+package com.generics.wildcards.exercise4;
+
+import com.generics.wildcards.generic.Shape;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyArrayApp {
+
+  public static <T> void copy(List<? extends T> source, List<? super T> destination) {
+
+    destination.addAll(source);
+  }
+
+  public static void main(String[] args) {
+
+    List<Integer> source = new ArrayList<>();
+    source.add(3);
+    source.add(4);
+
+    List<Integer> destination = new ArrayList<>();
+    copy(source, destination);
+    System.out.println(destination);
+  }
+
+}
