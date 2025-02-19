4573bae01c1bc3ea6868b623d935cf1c3ff6e35e
albertofullstack <alberto.fullstack@gmail.com>
2025-02-19 22:11:55 +0100

    Implements exercise 6.

---
+++ generics-demo/src/com/generics/lists/ArrayListExample.java
@@ -0,0 +1,49 @@
+package com.generics.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+
+  public static void main(String[] args) {
+
+    // resizing the array takes O(N)
+    List<String> names = new ArrayList<>();
+    names.add("Juan");
+    names.add("Kevin");
+    names.add("Bartolo");
+
+    // random indexing in O(1)
+    System.out.println("The name in the index 0 is " + names.get(0));
+
+    System.out.println();
+    // insert item in a given index - O(N)
+    names.add(2, "Daniel");
+
+
+    System.out.println("Elements of the list");
+    // because of the Iterable interface
+    for (String s : names) {
+      System.out.println(s);
+    }
+
+    // ARRAYS ARE FAST - O(1) IF WE REMOVE THE LAST ITEM, OR WE INSERT IN THE LAST POSITION
+
+    System.out.println();
+    // removes Juan in index 0 - O(N)
+    names.remove(0);
+    System.out.println("Items after removal of index 0");
+    System.out.println(names);
+
+    System.out.println();
+    System.out.println("Elements in array");
+    Object[] o = names.toArray();
+    for (Object element : o) {
+      System.out.println(element);
+    }
+
+    // contains will check whether the item is present on the array O(N)
+    System.out.println(names.contains("Kate"));
+    System.out.println(names.contains("Daniel"));
+  }
+}
---
+++ generics-demo/src/com/generics/lists/exercise6/ReverseList.java
@@ -0,0 +1,36 @@
+package com.generics.lists.exercise6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReverseList {
+
+  public static void reverse(List<Integer> list) {
+
+    for (int i = 0; i < list.size()/2; i++) {
+      int aux = list.get(i);
+      list.set(i, list.get(list.size() - i - 1));
+      list.set(list.size() - i - 1, aux);
+    }
+  }
+
+  public static void main(String[] args) {
+
+    List<Integer> ints = Arrays.asList(2, 3, 4, 5, 6);
+    System.out.println("List: " + ints);
+    reverse(ints);
+    System.out.println("Reversed list: " + ints);
+
+    System.out.println();
+    List<Integer> ints2 = Arrays.asList(2, 3, 4, 6);
+    System.out.println("List2: " + ints2);
+    reverse(ints2);
+    System.out.println("Reversed list 2: " + ints2);
+
+    System.out.println();
+    List<Integer> oneElementList = Arrays.asList(1);
+    reverse(oneElementList);
+    System.out.println(oneElementList);
+  }
+
+}
