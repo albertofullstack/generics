25b1126de6d0ec17319a3bd97bb32c7cc314318c
albertofullstack <alberto.fullstack@gmail.com>
2025-02-21 20:12:10 +0100

    Add stack example.

---
+++ generics-demo/src/com/generics/lists/StackExample.java
@@ -0,0 +1,22 @@
+package com.generics.lists;
+
+import java.util.Stack;
+
+public class StackExample {
+
+  public static void main(String[] args) {
+    Stack<String> stringStack = new Stack<>();
+    stringStack.push("hola");
+    stringStack.push("amigo");
+    System.out.println(stringStack);
+
+    System.out.println("Peek: " + stringStack.peek());
+    System.out.println(stringStack.pop());
+    System.out.println(stringStack);
+    System.out.println("Size: " + stringStack.size());
+    stringStack.pop();
+    System.out.println(stringStack);
+    System.out.println("isEmpty stack: " + stringStack.isEmpty());
+  }
+
+}
