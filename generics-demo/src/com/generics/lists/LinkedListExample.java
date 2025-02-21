72f7a9601609184be25ba2b5155d2b771852e1d4
albertofullstack <alberto.fullstack@gmail.com>
2025-02-21 19:35:51 +0100

    Add linkedlist example.

---
+++ generics-demo/src/com/generics/lists/LinkedListExample.java
@@ -0,0 +1,36 @@
+package com.generics.lists;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+
+  public static void main(String[] args) {
+
+    // USUALLY WE USE LINKED LIST (MORE MEMORY!!!)
+    LinkedList<Integer> nums = new LinkedList<>();
+    nums.add(1);
+    nums.add(3);
+    nums.add(10);
+    nums.add(5);
+
+    for (Integer num : nums) {
+      System.out.println(num);
+    }
+
+    System.out.println();
+    nums.addFirst(11);
+    nums.addFirst(20);
+    nums.addFirst(40);
+    System.out.println(nums);
+
+    nums.removeFirst();
+    System.out.println(nums);
+
+    nums.removeLast();
+    System.out.println(nums);
+    System.out.println("Size: " + nums.size());
+    System.out.println("isEmpty: " + nums.isEmpty());
+  }
+
+}
