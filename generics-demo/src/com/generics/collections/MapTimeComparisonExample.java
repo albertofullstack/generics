254c53409cdaf4370196a9494b1835e1ed8526cf
albertofullstack <alberto.fullstack@gmail.com>
2025-02-23 13:34:04 +0100

    Modify class name.

--- generics-demo/src/com/generics/collections/MapTimeComparisonExample.java
+++ generics-demo/src/com/generics/collections/MapTimeComparisonExample.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class TreeTimeComparisonExample {
+public class MapTimeComparisonExample {
 
   public static void main(String[] args) {
 
